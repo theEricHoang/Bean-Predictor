2f4e5285761b3af90f417486abe7e27f264cc38b
theboysack <ehoang2@student.gsu.edu>
2023-04-11 13:43:51 -0400

    added stuff

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ import seaborn as sb
 import os
 
 data = pd.read_excel(os.path.join(os.path.dirname(__file__), 'Dry_Bean_Dataset.xlsx'))
+dataClean = data.dropna() # clean data
+dataByClass = data.groupby('Class')
+
+# splitting data by class of bean
+seker = dataByClass.get_group('SEKER')
+barbunya = dataByClass.get_group('BARBUNYA')
+bombay = dataByClass.get_group('BOMBAY')
+cali = dataByClass.get_group('CALI')
+dermosan = dataByClass.get_group('DERMOSAN')
+horoz = dataByClass.get_group('HOROZ')
+sira = dataByClass.get_group('SIRA')
+
+# TODO: gather stats for 2 or 3 classes of beans. only need to do mean, median, variance, std, etc. for AREA, PERIMETER, and ROUNDNESS
+
+# TODO: Charts
+
+# TODO: Regression/Classification model
