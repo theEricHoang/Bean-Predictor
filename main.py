50a97d75309b50fde39d3f9b4008f97d52e83959
theboysack <ehoang2@student.gsu.edu>
2023-04-11 13:16:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy
-import pandas
-import matplotlib
-import seaborn
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import seaborn as sb
 import os
+
+data = pd.read_excel(os.path.join(os.path.dirname(__file__), 'Dry_Bean_Dataset.xlsx'))
